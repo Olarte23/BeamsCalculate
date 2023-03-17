20763457c4ab71bd8465e8c3f8335fe443d41af3
CRISTIAN OLARTE CARRASQUILLA <cristianolarte321414@correo.itm.edu.co>
2023-03-16 22:01:38 -0500

    Add project files.

---
+++ Beam.Logic/Logic.cs
@@ -0,0 +1,94 @@
+﻿namespace Beam.Logic
+{
+    public class Logic
+    {
+        private readonly string _txt;
+
+        public Logic(string txt)
+        {
+            _txt = txt;
+        }
+        public String Validator()
+        {
+
+            if (!(_txt[0] == '%' || _txt[0] == '&' || _txt[0] == '#'))
+            {
+                return "La viga esta mal construida!";
+            }
+
+            for (int i = 1; i < _txt.Length ; i++)
+            {
+                if (!(_txt[i].Equals('=') || _txt[i].Equals('*')))
+                {
+                    return "La viga esta mal construida!";
+                }
+            }
+            for (int i = 1; i < _txt.Length - 1; i++)
+            {
+                if (_txt[i].Equals('*') && _txt[i + 1].Equals('*'))
+                {
+                    return "La viga esta mal construida!";
+                }
+
+            }
+            return "Beam correct";
+
+        }
+        public int BeamResistence()
+        {
+            int baseResistence = 0;
+            int weigthBeam = 0;
+            int count = 0;
+            int TotalWeigth = 0;
+
+
+            if (_txt[0] == '%')
+            {
+                baseResistence = 10;
+            }
+            if (_txt[0] == '&')
+            {
+                baseResistence = 30;
+            }
+            if (_txt[0] == '#')
+            {
+                baseResistence = 90;
+            }
+
+            for (int i = 1; i < _txt.Length - 1; i++)
+            {
+                if (_txt[i].Equals('='))
+                {
+                    count++;
+                    weigthBeam += count;
+                }
+                else
+                {
+                    TotalWeigth += weigthBeam;
+                    TotalWeigth += (weigthBeam * 2);
+                    count = 0;
+                    weigthBeam = 0;
+                }
+            }
+            return baseResistence - weigthBeam;
+        }
+
+        public string ValidateResistence()
+        {
+            if (Validator().Equals("Beam correct"))
+            {
+                if (BeamResistence() < 0)
+                {
+                    return "La viga NO soporta el peso";
+                }
+                else
+                {
+                    return "La viga resiste!";
+                }
+            }
+            return "";
+            
+        }
+
+    }
+}
---
+++ Exercise.UI.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using Beam.Logic;
+using System;
+
+Console.WriteLine("Enter your beam to calculate: \n");
+
+String? n = Console.ReadLine();
+
+var beam = new Logic($"{n}");
+
+Console.WriteLine(beam.Validator());
+Console.WriteLine(beam.ValidateResistence());
